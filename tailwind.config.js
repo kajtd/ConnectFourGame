87353aec1f5d7f3930bfebc97a3385727063503b
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-08 13:58:23 +0100

    add tailwind color and box shadow variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'royal-purple': '#5C2DD5',
+        'electric-violet': '#7945FF',
+        'salmon-pink': '#FD6687',
+        'mustard-yellow': '#FFCE67',
+      },
+      boxShadow: {
+        'primary': '0 10px 10px #000',
+      }
+    }
   },
   plugins: [],
 }
