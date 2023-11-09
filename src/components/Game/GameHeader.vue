539fffa0bdf38c0447dd24390333db7b83ece27e
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-09 19:22:20 +0100

    add working header to the game

--- src/components/Game/GameHeader.vue
+++ src/components/Game/GameHeader.vue
@@ -1,7 +1,26 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useGameStore } from '../../store/game'
+
+const gameStore = useGameStore()
+const { handleRestart } = gameStore;
+const { isGameOn } = storeToRefs(gameStore)
+</script>
+
 <template>
     <header class="flex items-center justify-between max-w-[632px] mx-auto w-full mt-12">
-        <button class="bg-royal-purple rounded-[20px] text-white uppercase py-[10px] px-5">Menu</button>
+        <button 
+            class="bg-royal-purple rounded-[20px] text-white uppercase py-[10px] px-5"
+            @click="isGameOn = false"
+        >
+            Menu
+        </button>
         <img src="./../../assets/images/logo.svg" alt="">
-        <button class="bg-royal-purple rounded-[20px] text-white uppercase py-[10px] px-5">Restart</button>
+    <button 
+        class="bg-royal-purple rounded-[20px] text-white uppercase py-[10px] px-5"
+        @click="handleRestart"
+    >
+        Restart
+    </button>
     </header>
 </template>
