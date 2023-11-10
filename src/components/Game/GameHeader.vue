82f5a2118ca6e04f2d63b0f8c126068b7a32bddd
Kajetan Domagala <kajtekd8@gmail.com>
2023-11-10 18:52:35 +0100

    change color on hover in game header buttons

--- src/components/Game/GameHeader.vue
+++ src/components/Game/GameHeader.vue
@@ -8,16 +8,16 @@ const { isGameOn } = storeToRefs(gameStore)
 </script>
 
 <template>
-    <header class="flex items-center justify-between max-w-[632px] mx-auto w-full mt-12">
+    <header class="flex items-center justify-between max-w-[632px] mx-auto w-full mt-12 px-5 md:px-0">
         <button 
-            class="bg-royal-purple rounded-[20px] text-white uppercase py-[10px] px-5"
+            class="bg-royal-purple hover:bg-salmon-pink transition-colors rounded-[20px] text-white uppercase py-[10px] px-5"
             @click="isGameOn = false"
         >
             Menu
         </button>
         <img src="./../../assets/images/logo.svg" alt="">
     <button 
-        class="bg-royal-purple rounded-[20px] text-white uppercase py-[10px] px-5"
+        class="bg-royal-purple hover:bg-salmon-pink transition-colors rounded-[20px] text-white uppercase py-[10px] px-5"
         @click="handleRestart"
     >
         Restart
